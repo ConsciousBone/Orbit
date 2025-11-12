6b73746fdd063c4d30d70e01a57b7518cb75f5a1
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-12 18:09:31 +0000

    Add greeting section

--- Orbit/Views/HomeView.swift
+++ Orbit/Views/HomeView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct HomeView: View {
+    var greeting: String {
+        let hour = Calendar.current.component(.hour, from: Date())
+        
+        switch hour {
+        case 3..<12:
+            return "Good morning!"
+        case 12..<17:
+            return "Good afternoon!"
+        default:
+            return "Good evening!"
+        }
+    }
+    
     var body: some View {
-        Text("Home view")
+        Form {
+            Section {
+                let date = Date()
+                let formattedDate = date.formatted(date: .complete, time: .omitted)
+                VStack(alignment: .leading) {
+                    Text(greeting)
+                        .font(.largeTitle)
+                    Text("Today's date is \(formattedDate).")
+                        .font(.callout)
+                        .foregroundStyle(.secondary)
+                }
+            } header: {
+                Text("placeholder")
+            }
+            .listRowSeparator(.hidden)
+        }
     }
 }
 
