4e5da36eb6d03b85ac0c3748a559c82cc80d7c2a
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-12 13:47:31 +0000

    Initial Commit

---
+++ Orbit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orbit
+//
+//  Created by Evan Plant on 12/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by Evan Plant on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
