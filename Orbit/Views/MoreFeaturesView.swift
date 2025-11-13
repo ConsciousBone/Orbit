e06f5d6a00e960600ad106e1c2492618c23d1cec
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-13 08:49:35 +0000

    Add buttons to smaller features in morefeaturesview

--- Orbit/Views/MoreFeaturesView.swift
+++ Orbit/Views/MoreFeaturesView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct MoreFeaturesView: View {
     var body: some View {
-        Text("More features view")
+        NavigationStack {
+            Form {
+                Section {
+                    NavigationLink {
+                        Text("people in space thing")
+                    } label: {
+                        Label("People in space", systemImage: "figure")
+                    }
+                } footer: {
+                    Text("See who's currently in space, and what spacecraft they're on!")
+                }
+                
+                Section {
+                    NavigationLink {
+                        Text("space jokes thing")
+                    } label: {
+                        Label("Space jokes", systemImage: "theatermasks")
+                    }
+                } footer: {
+                    Text("Have a laugh at some space themed jokes!")
+                }
+                
+                Section {
+                    NavigationLink {
+                        Text("spacebar counter thing")
+                    } label: {
+                        Label("Spacebar click counter", systemImage: "space")
+                    }
+                } footer: {
+                    Text("See how many times you can tap the *space*bar!")
+                }
+            }
+        }
     }
 }
 
