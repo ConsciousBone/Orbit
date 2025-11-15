ec21a1562ba0c58eb558dc6f0cf66342c4fced2b
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-15 19:07:48 +0000

    Make accentcolours into their own file
    Omg why didnt i think of this earlier

---
+++ Orbit/Backend/AccentColours.swift
@@ -0,0 +1,25 @@
+//
+//  AccentColours.swift
+//  Orbit
+//
+//  Created by Evan Plant on 15/11/2025.
+//
+
+import Foundation
+import SwiftUI
+
+let accentColours: [AnyGradient] = [
+    Color.red.gradient, Color.orange.gradient,
+    Color.yellow.gradient, Color.green.gradient,
+    Color.mint.gradient, Color.blue.gradient,
+    Color.purple.gradient, Color.brown.gradient,
+    Color.white.gradient, Color.black.gradient
+]
+
+let baseAccentColours: [Color] = [
+    .red, .orange,
+    .yellow, .green,
+    .mint, .blue,
+    .purple, .brown,
+    .white, .black
+]
