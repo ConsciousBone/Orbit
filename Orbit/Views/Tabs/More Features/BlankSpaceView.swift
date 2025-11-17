67794b9d744806810cba0e0bc2cdc7613e54c996
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-17 01:57:08 +0000

    Add texteditor + bg to blank space

--- Orbit/Views/Tabs/More Features/BlankSpaceView.swift
+++ Orbit/Views/Tabs/More Features/BlankSpaceView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct BlankSpaceView: View {
+    @AppStorage("blankSpaceText") private var blankSpaceText = ""
+    
     var body: some View {
-        Text("Blank space view")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.ultraThinMaterial)
+            TextEditor(text: $blankSpaceText)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+                .scrollContentBackground(.hidden) // https://stackoverflow.com/a/62848618
+                .padding()
+        }
+        .frame(width: 300, height: 500)
+        .background {
+            Image("StarrySky")
+                .scaledToFill()
+        }
     }
 }
 
